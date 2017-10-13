a23202f216e6814eba4dc697f77a871934122e22
rdrew <robdrewupei@gmail.com>
2017-10-13 10:06:25 -0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ options.basetheme = {
 
 // Set the URL used to access the Drupal website under development. This will
 // allow Browser Sync to serve the website and update CSS changes on the fly.
-options.drupalURL = '';
+options.drupalURL = 'https://curiosity.discoveryspace.ca';
 //options.drupalURL = 'http://social.dev:32780';
 
 // Define the node-sass configuration. The includePaths is critical!
